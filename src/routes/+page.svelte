0dd74e18900a116421fa572acc811ffc4892854f
lia-07 <107384739+lia-07@users.noreply.github.com>
2023-08-09 13:19:18 +1200

    make the fade only show if the user has typed more than 500 chars

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,10 +46,11 @@
 </script>
 
 <div class="flex h-screen w-full">
-  <form class="relative flex h-full flex-1 flex-col">
-    <div
-      class=" pointer-events-none absolute inset-0 h-80 w-full bg-gradient-to-b from-base03 to-transparent"
-    />
+    {#if textInput?.length > 500}
+      <div
+        class=" pointer-events-none absolute inset-0 h-80 w-full bg-gradient-to-b from-base03 to-transparent"
+      />
+    {/if}
     <textarea
       class="w-full flex-1 resize-none scroll-pb-16 overflow-y-auto scroll-smooth whitespace-pre-wrap border-none bg-transparent pb-32 pl-4 pr-8 pt-32 font-supreme text-xl leading-8 tracking-wider text-opacity-75 outline-none focus:ring-0"
       placeholder="Start typing what you dreamed..."
