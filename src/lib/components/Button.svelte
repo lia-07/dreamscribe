1fa97e052c5155e93a893e687ea2d846acbbd990
lia-07 <107384739+lia-07@users.noreply.github.com>
2023-08-09 12:58:28 +1200

    update button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -7,7 +7,7 @@
   class:btn-secondary={variant == 'secondary'}
   class:btn-destructive={variant == 'destructive'}
   class:btn-disabled={variant == 'disabled'}
-  class="flex h-8 min-w-[5rem] items-center justify-center gap-2 rounded px-4 font-bold shadow transition-all hover:opacity-80 hover:shadow-xl focus:scale-90 focus:opacity-80 active:scale-90"
+  class="flex h-10 min-w-[5rem] items-center justify-center gap-2 rounded px-4 font-bold shadow transition-all hover:opacity-80 hover:shadow-xl focus:scale-90 focus:opacity-80 active:scale-90"
 >
   <slot name="icon" />
 
