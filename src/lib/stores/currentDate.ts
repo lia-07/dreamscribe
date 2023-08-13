d0a9f9e01b3be6d1cc5e539e254fdc38156158bf
lia-07 <107384739+lia-07@users.noreply.github.com>
2023-08-13 18:56:55 +1200

    fixed bug

--- src/lib/stores/currentDate.ts
+++ src/lib/stores/currentDate.ts
@@ -1,4 +1,4 @@
 import { readable } from 'svelte/store';
 import { formatTimestamp } from '$lib/scripts/timestamp';
 
-export const currentDate = readable<String>(formatTimestamp(Date.now()));
+export const currentDate = readable<string>(formatTimestamp(Date.now()));
