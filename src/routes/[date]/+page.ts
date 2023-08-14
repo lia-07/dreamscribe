8479601e50bd104b3373c5611b95edcf2a5c71a5
lia-07 <107384739+lia-07@users.noreply.github.com>
2023-08-15 11:46:50 +1200

    add error handling to page.ts for the journal entry view route

--- src/routes/[date]/+page.ts
+++ src/routes/[date]/+page.ts
@@ -1,4 +1,5 @@
 import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
 import { get } from 'svelte/store';
 import { journalEntries } from '$lib/stores/journalEntries';
@@ -10,9 +11,14 @@ export const load: PageLoad = ({ params }) => {
   if (matchingIndex != -1) {
     return {
       journalEntry: {
+        date: get(journalEntries)[matchingIndex].date,
         content: get(journalEntries)[matchingIndex].content,
         mood: get(journalEntries)[matchingIndex].mood
       }
     };
+  } else {
+    throw error(404, {
+      message: 'Unfortunately, the journal entry you requested could not be found'
+    });
   }
 };
